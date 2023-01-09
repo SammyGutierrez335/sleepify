065e934a59a4225a0b41bc3b5de0c8d5ee2ab782
Sam Gutierrez <samgutierrez335@gmail.com>
2023-01-09 11:36:25 -0600

    updates comments in Artist and Song Schema

--- models/Song.js
+++ models/Song.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // db.songs.insertMany([
-//   { title: "All At Once", genre: "Soft Rock", artist: "5e42ca083ce1c82e3858d128", imageUrl: "https://sleepify-dev.s3.us-east-2.amazonaws.com/artists/jack_johnson.jpg", songUrl: "https://sleepify-dev.s3.us-east-2.amazonaws.com/01+All+At+Once.mp3"},
-//   { title: "Father and Son", genre: "Classic Rock", artist: "5e42ca083ce1c82e3858d12a", imageUrl: "https://sleepify-dev.s3.us-east-2.amazonaws.com/artists/cat_stevens.jpg", songUrl: "https://sleepify-dev.s3.us-east-2.amazonaws.com/Cat+Stevens+-+02+-+Father+And+Son.mp3" },
-//   { title: "I Got You", genre: "Soft Rock", artist: "5e42ca083ce1c82e3858d128", imageUrl: "https://sleepify-dev.s3.us-east-2.amazonaws.com/artists/jack_johnson.jpg", songUrl: "https://sleepify-dev.s3.us-east-2.amazonaws.com/Jack+Johnson+-+01+-+I+Got+You.mp3"},
-//   { title: "New Light", genre: "Alternative Rock", artist: "5e42ca083ce1c82e3858d129", imageUrl: "https://sleepify-dev.s3.us-east-2.amazonaws.com/artists/john_mayer.jpg", songUrl: "https://sleepify-dev.s3.us-east-2.amazonaws.com/John+Mayer+-+01+-+New+Light.mp3" }
+//   { "title": "All At Once", "genre": "Soft Rock", "artist": "5e42ca083ce1c82e3858d128", "imageUrl": "https://sleepify-dev.s3.us-east-2.amazonaws.com/artists/jack_johnson.jpg", "songUrl": "https://sleepify-dev.s3.us-east-2.amazonaws.com/01+All+At+Once.mp3"},
+//   { "title": "Father and Son", "genre": "Classic Rock", "artist": "5e42ca083ce1c82e3858d12a", "imageUrl": "https://sleepify-dev.s3.us-east-2.amazonaws.com/artists/cat_stevens.jpg", "songUrl": "https://sleepify-dev.s3.us-east-2.amazonaws.com/Cat+Stevens+-+02+-+Father+And+Son.mp3" },
+//   { "title": "I Got You", "genre": "Soft Rock", "artist": "5e42ca083ce1c82e3858d128", "imageUrl": "https://sleepify-dev.s3.us-east-2.amazonaws.com/artists/jack_johnson.jpg", "songUrl": "https://sleepify-dev.s3.us-east-2.amazonaws.com/Jack+Johnson+-+01+-+I+Got+You.mp3"},
+//   { "title": "New Light", "genre": "Alternative Rock", "artist": "5e42ca083ce1c82e3858d129", "imageUrl": "https://sleepify-dev.s3.us-east-2.amazonaws.com/artists/john_mayer.jpg", "songUrl": "https://sleepify-dev.s3.us-east-2.amazonaws.com/John+Mayer+-+01+-+New+Light.mp3" }
 // ]);
 
-//
 const SongSchema = new Schema({
   title: {
     type: String,
