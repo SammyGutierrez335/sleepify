db02b68f21457a41ae02a159e47d195614ddc8b7
Sammy Gutierrez <samgutierrez335@gmail.com>
2025-08-22 21:18:54 -0600

    additional tweaks to env variables

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const keys = require('../config/keys');
 const {hashPassword} = require('../helpers/bcrypt')
 const {findUser, createUser} = require('../services/user.service')
 
@@ -42,7 +41,7 @@ router.post('/login', async (req, res) => {
 
   jwt.sign(
     payload,
-    keys.secretOrKey,
+    process.env.SECRET_OR_KEY,
     // Tell the key to expire in one hour
     { expiresIn: 3600 },
     (err, token) => {
